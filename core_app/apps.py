93f6fd920f98307badb5f9219d4390e153bf608b
salman <mrsalmanxzs@gmail.com>
2025-11-22 12:47:52 +0530

    first commit

---
+++ core_app/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class CoreAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core_app'
+
+    def ready(self):
+        import core_app.signals
---
+++ core_app/signals.py
@@ -0,0 +1,30 @@
+from .models import Students
+from django.db.models.signals import post_save, pre_save, post_delete
+from django.dispatch import receiver
+from django.conf import settings
+from django.core.mail import send_mail
+
+@receiver(post_save, sender=Students)
+def student_created(sender, instance, created, **kwargs):
+    if created:
+        print("signals fired new student created:", instance.name)
+
+
+        if instance.email:
+            subject = "Welcome Student!"
+            message = f"Hi {instance.name}, Your student account has been created."
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                ["test@gmail.com"],
+                fail_silently=True,
+            )
+
+@receiver(pre_save, sender=Students)
+def student_pre_save(sender, instance, **kwargs):
+    print("pre save signal this about to save:", instance.name)
+
+@receiver(post_delete, sender=Students)
+def student_post_delete(sender, instance, **kwargs):
+    print("Post Delete Student deleted:", instance.name)
